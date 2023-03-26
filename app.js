1cf67677854d3a836300e7ba055ddada9828a3da
Yash Ruhil <74950632+YR047@users.noreply.github.com>
2023-03-26 16:08:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+/* QuerySelector */
 var inputs = document.querySelectorAll("#otp_container input");
-		var button = document.querySelector("#otp_container button");
+var button = document.querySelector("#otp_container button");
 
 		for (var i = 0; i < inputs.length; i++) {
 			inputs[i].addEventListener("input", function() {
